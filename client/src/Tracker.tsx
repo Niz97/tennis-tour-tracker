d4ea1af8e6ad8180a88776aeb0df0b4eb48d57a5
Niz97 <97niran@gmail.com>
2023-01-28 00:21:52 +0000

    Working on head to head score display

--- client/src/Tracker.tsx
+++ client/src/Tracker.tsx
@@ -4,7 +4,7 @@ import { useParams } from "react-router-dom";
 import { TTracker } from './api/getTrackers';
 
 function Tracker() {
-  const [tracker, setTracker] = useState<TTracker>();
+  const [tracker, setTracker] = useState<any>('');
   const { trackerId } = useParams();
   
   useEffect(() => {
@@ -12,10 +12,31 @@ function Tracker() {
       const tracker = await getTracker(trackerId!);
       setTracker(tracker);
     }
-  })
+    fetchTracker();
+  }, [])
 
   return (
-    <div>{tracker!}</div>
+    <div>
+      <div className="tracker-name">Name:{tracker.name}</div>
+      <div className="head-to-head-container">
+        <div style={{display:'flex'}} className="player-1">
+          <div className="player-name">Muray:</div>
+          <div className="score-container">
+            <div className="set-1">1</div>
+            <div className="set-1">2</div>
+            <div className="set-1">3</div>
+          </div>
+        </div>
+        <div style={{display:'flex'}} className="player-2">
+          <div className="player-name">Nadal:</div>
+          <div className="score-container">
+            <div className="set-1">2</div>
+            <div className="set-1">4</div>
+            <div className="set-1">6</div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
