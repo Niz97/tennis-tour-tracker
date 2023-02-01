df9b844728b0bfb7680030774ad6792833be33ba
Niz97 <97niran@gmail.com>
2023-02-01 23:00:14 +0000

    Changing to show individual player ranking/live points

--- client/src/Tracker.tsx
+++ client/src/Tracker.tsx
@@ -4,7 +4,7 @@ import { useParams } from "react-router-dom";
 import { TTracker } from './api/getTrackers';
 
 function Tracker() {
-  const [tracker, setTracker] = useState<any>('');
+  const [tracker, setTracker] = useState<TTracker | any>('');
   const { trackerId } = useParams();
   
   useEffect(() => {
@@ -18,24 +18,10 @@ function Tracker() {
   return (
     <div>
       <div className="tracker-name">Name:{tracker.name}</div>
-      <div className="head-to-head-container">
-        <div style={{display:'flex'}} className="player-1">
-          <div className="player-name">Muray:</div>
-          <div className="score-container">
-            <div className="set-1">1</div>
-            <div className="set-1">2</div>
-            <div className="set-1">3</div>
-          </div>
-        </div>
-        <div style={{display:'flex'}} className="player-2">
-          <div className="player-name">Nadal:</div>
-          <div className="score-container">
-            <div className="set-1">2</div>
-            <div className="set-1">4</div>
-            <div className="set-1">6</div>
-          </div>
-        </div>
-      </div>
+      <div>Ranking: </div>
+      <div>Live Points:</div>
+      <div>Official Points:</div>
+
     </div>
   )
 }
